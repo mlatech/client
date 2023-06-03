8fff8132053ebab92f998fcbdf53f3431d096d60
Karyna Chernyak <mlatech@icloud.com>
2023-06-03 18:50:28 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react"
+import axios from "axios"
+import Movie from "./components/Movie.js"
+import AddMovieForm from "./components/AddMovieForm.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    const [movies, setMovies] = useState([])
+
+    function getMovies(){
+        axios.get("/movies")
+        .then(res => setMovies(res.data))
+        .catch(err => console.log(err.response.data.errMsg))
+    }
+//
+    function addMovie(newMovie){
+        axios.post("/movies", newMovie)
+        .then(res => {
+            setMovies(prevMovies => [...prevMovies, res.data])
+        })
+        .catch(err => console.log(err))
+
+    
+    }
+//delete
+    function deleteMovie(movieId){
+        axios.delete(`/movies/${movieId}`)
+        .then(res => {
+            setMovies(prevMovies => prevMovies.filter(movie => movie._id !== movieId))
+            })
+    }
+//edit 
+function editMovie(updates, movieId){
+    axios.put(`/movies/${movieId}`, updates)
+    .then(res => {
+        setMovies(prevMovies => prevMovies.map(movie => (movie._id === movieId ? res.data : movie)))
+    })
+    .catch(err => console.log(err))
+}
+    function handleFilter(e){
+        if(e.target.value === "reset"){
+            getMovies()
+        }
+        else {
+        axios.get(`/movies/search/genre?genre=${e.target.value}`)
+        .then(res => setMovies(res.data))
+        .catch(err => console.log(err))
+    }
 }
 
-export default App;
+
+    useEffect(()=>{
+        getMovies()
+    }, [])
+    return(
+        <div className="movie-container">
+            <AddMovieForm
+            submit={addMovie}
+            btnText="Add Movie"
+            />
+            <h4>Filter By Genre</h4>
+            <select onChange={handleFilter} className="filter-form">
+                <option value="reset">All Movies</option>
+                <option value="action">Action</option>
+                <option value="fantasy">Fantasy</option>
+                <option value="horror">Horror</option>
+            </select>
+
+            {movies.map(movie => <Movie 
+            {...movie} 
+            key={movie.title}
+            deleteMovie = {deleteMovie}
+            editMovie = {editMovie}
+            />)}
+        </div>
+    )
+}
